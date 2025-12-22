1d12f74a0b960b849d8be8ebf565c31a1f4a10b5
Santiago Munez <losanonimostube8@gmail.com>
2025-12-22 01:01:12 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == "admin" and password == "admin":  # Ejemplo simple
+            return redirect(url_for('reservas'))
+        else:
+            return "Datos incorrectos"
+    return render_template('login.html')
+
+@app.route('/reservas')
+def reservas():
+    return render_template('reservas.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
